ba4949763e792cb28fab7ad9f7f548e8757b9a8b
vpaul007 <96490757+vpaul007@users.noreply.github.com>
2025-04-04 17:55:39 +0100

    Transaction.java

--- Transaction.java
+++ Transaction.java
@@ -1,5 +1,12 @@
 package atmAdvance1;
 
-public sealed interface Transaction permits Deposit, Withdraw, BalanceCheck  {
 
-}
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public sealed interface Transaction permits Deposit, Withdraw, BalanceCheck  {
+    LocalDateTime timestamp();
+
+
+    }
+
