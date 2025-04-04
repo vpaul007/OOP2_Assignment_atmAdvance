3e188a1605de39a97a8e3d6da3660e43a2ee773c
vpaul007 <96490757+vpaul007@users.noreply.github.com>
2025-04-04 17:51:27 +0100

    ATMApp.java

--- ATMApp.java
+++ ATMApp.java
@@ -6,14 +6,27 @@ import java.util.List;
 public class ATMApp {
 	public static void main(String[] args) {
         ATMService atm = new ATMService();
+		
+        //Case - Default Account Creation
+        Account acc2 = atm.createDefaultAccount();
+        atm.addAccount(acc2);
+        System.out.println("\n-----------New Default Account-----------\n" + acc2);
 
+        //Case - User Account View
         Account acc1 = new Account("12345", "John Doe", BigDecimal.valueOf(1000));
         atm.addAccount(acc1);
-
+        atm.processTransaction("12345", new BalanceCheck());
+		
+         //Case - User Account View After Deposit
         atm.processTransaction("12345", new Deposit(BigDecimal.valueOf(500)));
+        atm.processTransaction("12345", new BalanceCheck());
+
+	 //Case - User Account View After Withdrawal
+
         atm.processTransaction("12345", new Withdraw(BigDecimal.valueOf(200)));
         atm.processTransaction("12345", new BalanceCheck());
 
+		
         List<Transaction> transactions = List.of(
             new Deposit(BigDecimal.valueOf(1000)),
             new Withdraw(BigDecimal.valueOf(3000)),
