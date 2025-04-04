ce0f7dcfa4d10a0a2eba52f6f2634214ccbdcb54
vpaul007 <96490757+vpaul007@users.noreply.github.com>
2025-04-04 17:57:36 +0100

    Withdraw.java

--- Withdraw.java
+++ Withdraw.java
@@ -1,7 +1,14 @@
-package atmAdvance1;
+package atmAdvance;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
-public record Withdraw(BigDecimal amount) implements Transaction {
+public record Withdraw(BigDecimal amount, LocalDateTime timestamp) implements Transaction {
+    public Withdraw(BigDecimal amount){
+        this(amount, LocalDateTime.now());}
 
+    @Override
+    public LocalDateTime timestamp() {
+        return LocalDateTime.now();
+    }
 }
