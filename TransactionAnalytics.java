57395b974704e9a4cd9e77a3955e41c892c65d0d
vpaul007 <96490757+vpaul007@users.noreply.github.com>
2025-04-04 17:57:02 +0100

    TransactionAnalytics.java

--- TransactionAnalytics.java
+++ TransactionAnalytics.java
@@ -8,14 +8,14 @@ public class TransactionAnalytics {
 	 public static void analyzeTransactions(List<Transaction> transactions) {
 	        // Count transactions
 	        long count = transactions.stream().count();
-	        System.out.println("Total Transactions: " + count);
+	        System.out.println("\n -------ATM Transactions-------\nTotal Transactions in ATM: " + count);
 
 	        // Find the max deposit
 	        transactions.stream()
 	            .filter(t -> t instanceof Deposit)
 	            .map(t -> ((Deposit) t).amount())
 	            .max(Comparator.naturalOrder())
-	            .ifPresent(max -> System.out.println("Max Deposit: " + max));
+	            .ifPresent(max -> System.out.println("Max Deposit in ATM: " + max));
 
 	        // Find if any withdrawal exceeds 5000
 	        boolean anyLargeWithdrawal = transactions.stream()
@@ -26,7 +26,11 @@ public class TransactionAnalytics {
 	        Map<Class<? extends Transaction>, List<Transaction>> groupedTransactions = transactions.stream()
 	            .collect(Collectors.groupingBy(Transaction::getClass));
 
-	        System.out.println("Grouped Transactions: " + groupedTransactions);
+	     System.out.println("\n--- Grouped Transactions --- ");
+			groupedTransactions.forEach((type, txns) -> {
+				System.out.println("\n" + type.getSimpleName() + "Transactions:");
+				txns.forEach(System.out::println);
+			});
 	    }
 	}
 
