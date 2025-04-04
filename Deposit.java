43ac22b74efeb142b36bca97b7a73170e601dff7
vpaul007 <96490757+vpaul007@users.noreply.github.com>
2025-04-04 17:55:12 +0100

    Deposit.java

--- Deposit.java
+++ Deposit.java
@@ -1,7 +1,16 @@
 package atmAdvance1;
 
+
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
-public record Deposit(BigDecimal amount) implements Transaction  {
+public record Deposit(BigDecimal amount, LocalDateTime timestamp) implements Transaction  {
 
+    public Deposit(BigDecimal amount){
+        this(amount, LocalDateTime.now());
+    }
+    @Override
+    public LocalDateTime timestamp() {
+        return LocalDateTime.now();
+    }
 }
